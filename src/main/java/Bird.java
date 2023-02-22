9a5c93bb07627747ee80c2b6b94225151295e346
Kodie Kuiper <antonio.pennyman@hyperisland.se>
2023-02-22 12:29:33 +0000

    return Bird Test

--- src/main/java/Bird.java
+++ src/main/java/Bird.java
@@ -31,6 +31,7 @@ public class Bird {
     /**
      * A variable (in this context, often referred to as a field), which represents the age of the bird.
      */
+
     public int ageMonths;
 
     /**
@@ -45,6 +46,11 @@ public class Bird {
      *         greater than or equal to 3.
      */
     public String sound(){
+        if(ageMonths < 3){
+            return ("Tweet");
+        } else if (ageMonths >= 3){
+            return ("Chirp");
+        }
         return "";
     }
 
